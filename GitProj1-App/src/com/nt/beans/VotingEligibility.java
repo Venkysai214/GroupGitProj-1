745b7ea9ac6de46ce0f33bd1443dc1853f7052cc
TL <Admin@192.168.1.239>
2019-04-01 15:58:39 +0530

    venky- added jars and bean class

--- GitProj1-App/src/com/nt/beans/VotingEligibility.java
+++ GitProj1-App/src/com/nt/beans/VotingEligibility.java
@@ -1,5 +1,27 @@
 package com.nt.beans;
 
 public class VotingEligibility {
-
+			private String name;
+			private int age;
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
+			public int getAge() {
+				return age;
+			}
+			public void setAge(int age) {
+				this.age = age;
+			}
+			
+			public String checkingEligibility() {
+				if(age>18)
+					return  name+"U r  eligible for voting";
+				else
+					return  name+" u r not eligible for voting";
+							
+			}
+	
 }
